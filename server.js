33a991783df836ea69b6ae2a2b95077147060d4c
Elkin <elkin@iMac-de-Nicolas.local>
2015-08-22 11:33:15 -0500

    Real time

--- server.js
+++ server.js
@@ -28,14 +28,14 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 //habilita cors en toda la api
-// app.use(cors());
- app.use(function (req, res, next){
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+app.use(cors());
+//  app.use(function (req, res, next){
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 // app.use(logger('dev'));
 app.use(methodOverride(function(req, res){
